d372a1e8659df2d15f255ddaf8f8b5964b85d443
Omar <omardraz94@gmail.com>
2021-12-12 15:17:49 -0800

    ADD: Finished tesla clone

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,54 @@ import Section from './Section'
 function Home() {
     return (
         <Container>
-            <Section/>
+            <Section
+                title="Model S"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="./images/model-s.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model Y"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="./images/model-y.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model 3"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="./images/model-3.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model X"
+                description="Order Online for Touchless Delivery"
+                backgroundImg="./images/model-x.jpg"
+                leftBtnText="Custom Order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Lowest Cost Solar Panels in America"
+                description="Money Back Guarantee"
+                backgroundImg="./images/solar-panel.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn More"
+            />
+            <Section
+                title="Solar for New Roofs"
+                description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+                backgroundImg="./images/solar-roof.jpg"
+                leftBtnText="Order Now"
+                rightBtnText="Learn More"
+            />    
+            <Section
+                title="Accessories"
+                description=""
+                backgroundImg="./images/Accessories.jpg"
+                leftBtnText="Shop Now"
+            />                        
         </Container>
     )
 }
